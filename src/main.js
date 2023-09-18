8e642734bccb7a634ff954da79138a960193b971
IrmaSB <i.solano.baca@gmail.com>
2023-09-18 09:43:18 -0600

    Sin cambios

--- src/main.js
+++ src/main.js
@@ -2,7 +2,9 @@ import { example } from './dataFunctions.js';
 import { renderItems } from './view.js';
 
 // import data from './data/lol/lol.js';
-import data from './data/pokemon/pokemon.js';
-// import data from './data/rickandmorty/rickandmorty.js';
+//import data from './data/pokemon/pokemon.js';
+ import data from './data/rickandmorty/rickandmorty.js';
 
 console.log(example, renderItems, data);
+
+
