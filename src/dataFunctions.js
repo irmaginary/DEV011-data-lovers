4d60c8113186055969c93d220e64f2f38eb4d7de
IrmaSB <i.solano.baca@gmail.com>
2023-09-19 12:09:29 -0600

    Function Sort OK. Falta arreglar Render Items

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,30 @@
 // estas funciones son de ejemplo
 
 export const example = () => {
-  return 'example';
+  return "example";
 };
 
 export const anotherExample = () => {
   return [];
 };
+
+//SORT-DATA
+export function sortData(data, sortBy, sortOrder) {
+  console.log(sortOrder);
+  if (sortOrder === "asc") {
+    return data.sort((a, b) => a[sortBy].localeCompare(b[sortBy]));
+  } else if (sortOrder === "desc") {
+    return data.sort((a, b) => b[sortBy].localeCompare(a[sortBy]));
+  } else {
+    throw new Error('El sortOrder debe ser "asc" o "desc".');
+  }
+};
+
+//FUNCION FILTRAR POR CATEGORIA "ESPECIE: HUMANO"
+//Definir función filter data
+//export const filterData= (data, filterBy, value) => {
+//return data.filter(item => item[filterBy] === value);
+//}
+//Filtrar cataegoría "species"
+//const filtercat= filterData (data, 'species', 'human');
+//console.log(filtercat);
--- src/main.js
+++ src/main.js
@@ -1,23 +1,59 @@
-import { example } from './dataFunctions.js';
-import { renderItems } from './view.js';
+import { example } from "./dataFunctions.js";
+import { renderItems } from "./view.js";
+import { sortData } from "./dataFunctions.js";
 
 // import data from './data/lol/lol.js';
-<<<<<<< HEAD
-//import data from './data/pokemon/pokemon.js';
- import data from './data/rickandmorty/rickandmorty.js';
 
-console.log(example, renderItems, data);
-
-
-=======
-
-import data from './data/rickandmorty/rickandmorty.js';
-const res = renderItems(data.results)
-console.log("res: ", res)
+//RENDERITEMS de la lista
+import data from "./data/rickandmorty/rickandmorty.js";
+const res = renderItems(data.results);
+//console.log("res: ", res);
 
 const ul = document.body.querySelector("#root ul");
 ul.innerHTML = res.join("");
-//reemplaza la información por el resultado, uniendo todos los arreglos.
+
+const ordenarresultados = sortData(data.results, 'name', 'asc');
+console.log(ordenarresultados);
+
+const selectsort = document.getElementById("select-sort");
+selectsort.addEventListener("change", function() {
+  console.log("Cambio");
+  const actualizarresultados = sortData(data.results, 'name', selectsort.value);
+  console.log(actualizarresultados);
+  return renderItems(actualizarresultados);
+} )
+
+
+
+//const selectfilter = document.getElementById("select-filter");
+
+//EVENT LISTENER para Ordenar resultados alfabéticamemte
+//const selectOrden = document.getElementById("select-sort");
+//selectOrden.addEventListener("click", function () {
+//    const ordenSeleccionado = selectOrden.value;
+
+//res.sort ((a,b) => {
+//    const nameA = a.textContent.toUpperCase();
+//  const nameB =b .textContent.toUpperCase();
+//    if (ordenSeleccionado === "asc") {
+//  if (nameA < nameB) return -1;
+//  if (nameA > nameB) return 1;
+//} else if (ordenSeleccionado === "desc") {
+  //  if (nameA > nameB) return -1;
+    //if (nameA < nameB) return 1;
+//}
+//    return 0;
+
+//});
+
+//const ul = document.body.querySelector("#root ul");
+//ul.innerHTML = res.map(item => item.outerHTML).join(""); //reemplaza la información por el resultado, uniendo todos los arreglos.
+//});
 
 //console.log(example, renderItems, data);
->>>>>>> 9b674b5dc50bd2be51692ff16c02bd563a1c2752
+
+
+
+
+
+
