5e4b4cd3e6f03c7d7a4cce9108d2267f4e539f24
IrmaSB <i.solano.baca@gmail.com>
2023-09-25 09:12:26 -0600

    Cambios aceptados/Sprint 3

--- src/main.js
+++ src/main.js
@@ -47,32 +47,7 @@ botonReset.addEventListener("click", limpiar);
 
 
 
-//const selectfilter = document.getElementById("select-filter");
 
-//EVENT LISTENER para Ordenar resultados alfabéticamemte
-//const selectOrden = document.getElementById("select-sort");
-//selectOrden.addEventListener("click", function () {
-//    const ordenSeleccionado = selectOrden.value;
-
-//res.sort ((a,b) => {
-//    const nameA = a.textContent.toUpperCase();
-//  const nameB =b .textContent.toUpperCase();
-//    if (ordenSeleccionado === "asc") {
-//  if (nameA < nameB) return -1;
-//  if (nameA > nameB) return 1;
-//} else if (ordenSeleccionado === "desc") {
-  //  if (nameA > nameB) return -1;
-    //if (nameA < nameB) return 1;
-//}
-//    return 0;
-
-//});
-
-//const ul = document.body.querySelector("#root ul");
-//ul.innerHTML = res.map(item => item.outerHTML).join(""); //reemplaza la información por el resultado, uniendo todos los arreglos.
-//});
-
-//console.log(example, renderItems, data);
 
 
 
