d7c918132f0f26147c33f3ea8b4431b619e96d88
IrmaSB <i.solano.baca@gmail.com>
2023-10-05 08:36:50 -0600

    Test unitarios: filterData funciona parcialmente

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -9,15 +9,12 @@ export function filtrarData(data, especie) {
     return item.species === especie;
   });
 
-  return result; 
+  return result;
 }
-
-
-
 
 // Función SORT-DATA
 export function sortData(data, sortBy, sortOrder) {
-  console.log(sortOrder);
+  //console.log(sortOrder);
   if (sortOrder === "asc") {
     return data.sort((a, b) => a[sortBy].localeCompare(b[sortBy]));
   } else if (sortOrder === "desc") {
@@ -57,18 +54,18 @@ export function computeStat(data) {
 }
 //Función datos random (estadística)
 //export function computeStat(data) {
-  //const sum = data.reduce((a, b) => a + b, 0);
-  //const average = sum / data.lenght;
-  //const max = data.reduce((a, b) => Math.max(a, b), -Infinity);
-  //const min = data.reduce((a, b) => Math.min(a, b), Infinity);
+//const sum = data.reduce((a, b) => a + b, 0);
+//const average = sum / data.lenght;
+//const max = data.reduce((a, b) => Math.max(a, b), -Infinity);
+//const min = data.reduce((a, b) => Math.min(a, b), Infinity);
 
-  //console.log(sum);
-  //return {
-    //sum: sum,
-    //average: average,
-    //max: max,
-    //min: min,
-  //};
+//console.log(sum);
+//return {
+//sum: sum,
+//average: average,
+//max: max,
+//min: min,
+//};
 //}
 
 /*export function computeStat(data) {
@@ -149,9 +146,3 @@ export function computeStat(data) {
 
   return datagender;
 }*/
-
-
-
-
- 
-
--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,19 +1,19 @@
 //import { anotherExample } from "../src/dataFunctions.js";
 import { data as fakeData } from "./data.js";
-import { filtrarData } from "./src/dataFunctions.js";
+import { filtrarData } from "../src/dataFunctions.js";
 
-console.log(fakeData);
+//console.log(fakeData);
 
 describe("filtrarData", () => {
   it('Devuelve todos los datos si se especifica "All"', () => {
     const especie = "All";
-    const resultado = filtrarData(data, especie);
-    expect(resultado).toEqual(data);
+    const resultado = filtrarData(fakeData, especie);
+    expect(resultado).toEqual(fakeData);
   });
 
   it("Devuelve sólo los datos de la especie seleccionada", () => {
     const especie = "Human";
-    const resultado = filtrarData(data, especie);
+    const resultado = filtrarData(fakeData, especie);
 
     const resultadoesperado = [
       { id: 1, species: "human" },
@@ -24,13 +24,13 @@ describe("filtrarData", () => {
 
   it("Devuelve un array vacío si la especie seleccionada no existe en los datos", () => {
     const especie = "perro"; // Una especie que no está en los datos
-    const resultado = filtrarData(data, especie);
+    const resultado = filtrarData(fakeData, especie);
     expect(resultado).toEqual([]);
   });
 });
 
 //describe("anotherExample", () => {
-  //it("returns `anotherExample`", () => {
-    //expect(anotherExample()).toBe("OMG");
-  //});
+//it("returns `anotherExample`", () => {
+//expect(anotherExample()).toBe("OMG");
+//});
 //});
