2097b541815ce40983a01968ba8b7f6c53e11201
IrmaSB <i.solano.baca@gmail.com>
2023-10-05 09:56:17 -0600

    Test unitario: filtrarData + sortData

--- test/dataFunctions.spec.js
+++ test/dataFunctions.spec.js
@@ -1,6 +1,7 @@
 //import { anotherExample } from "../src/dataFunctions.js";
 import { data as fakeData } from "./data.js";
 import { filtrarData } from "../src/dataFunctions.js";
+import { sortData } from "../src/dataFunctions.js";
 
 //console.log(fakeData);
 
@@ -12,14 +13,14 @@ describe("filtrarData", () => {
   });
 
   it("Devuelve sólo los datos de la especie seleccionada", () => {
-    const especie = "Human";
+    const especie = "human";
     const resultado = filtrarData(fakeData, especie);
 
     const resultadoesperado = [
       { id: 1, species: "human" },
       { id: 3, species: "human" },
     ];
-    expect(resultado).toEqual(resultadoesperado);
+    expect(resultado).toBe(resultadoesperado);
   });
 
   it("Devuelve un array vacío si la especie seleccionada no existe en los datos", () => {
@@ -28,6 +29,43 @@ describe("filtrarData", () => {
     expect(resultado).toEqual([]);
   });
 });
+
+//Caso de pueba 1: orden ascendente
+describe("sortData", () => {
+  it('Orden ascendente', () => {
+
+    const sortBy = 'nombre';
+    const sortOrder = 'asc';
+    const result = sortData(fakeData, sortBy, sortOrder);
+    // Verifica que la función devuelva un arreglo ordenado ascendente
+    expect(result).toEqual([
+      { id: 2, nombre: "Morty Smith" },
+      { id: 1, nombre: "Rick Sanchez" },
+      { id: 3, nombre: "Summer Smith" },
+    ]);
+  });
+  // Caso de prueba 2: Orden descendente (desc)
+  it('Orden descendente', () => {
+  
+    const sortBy = 'nombre';
+    const sortOrder = 'desc';
+    const result = sortData(fakeData, sortBy, sortOrder);
+    // Verifica que la función devuelva un arreglo ordenado descendente
+    expect(result).toEqual([
+      { id: 3, nombre: "Summer Smith" },
+      { id: 1, nombre: "Rick Sanchez" },
+      { id: 2, nombre: "Morty Smith" },
+    ]);
+  });
+});
+ 
+
+
+
+
+
+
+
 
 //describe("anotherExample", () => {
 //it("returns `anotherExample`", () => {
